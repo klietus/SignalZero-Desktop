03392095f152bb208a74ec0d001d83f6ef9d2056
klietus <klietus@gmail.com>
2026-04-12 00:57:15 +0200

    chore: release v1.1.6

--- Casks/signalzero-desktop.rb
+++ Casks/signalzero-desktop.rb
@@ -1,6 +1,6 @@
 cask "signalzero-desktop" do
-  version "1.1.5"
-  sha256 "e7b2425e9c7aa47b5c9a1b3e577b1a4eaf2db064e45d3a37c23eaa7599067706"
+  version "1.1.6"
+  sha256 "9ec85f151f4312737eef1b5d5c2923f51ad3b384b03ecce34781d3db6f30dccf"
 
   url "https://github.com/klietus/SignalZero-Desktop/releases/download/v#{version}/SignalZero-Desktop-#{version}-arm64.dmg"
   name "SignalZero-Desktop"
