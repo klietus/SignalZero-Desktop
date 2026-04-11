a436c46edfce9f5f2b03964dcfbbe06b2333803a
klietus <klietus@gmail.com>
2026-04-12 00:23:15 +0200

    chore: bump version to 1.1.4 and add explicit apache-arrow dependency

--- Casks/signalzero-desktop.rb
+++ Casks/signalzero-desktop.rb
@@ -1,6 +1,6 @@
 cask "signalzero-desktop" do
-  version "1.1.3"
-  sha256 "b40d8ce6cda9ae52b1b86347c0aae665e4b995d3f2da46220f0708385a4a399c"
+  version "1.1.4"
+  sha256 "9f0dfa80ceb201345cf54919701915730e59642726e7bcbc04ae5fe345c08a50"
 
   url "https://github.com/klietus/SignalZero-Desktop/releases/download/v#{version}/SignalZero-Desktop-#{version}-arm64.dmg"
   name "SignalZero-Desktop"
